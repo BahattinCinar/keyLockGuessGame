cac791db914341ca1129c929f00e270961326fe5
BahattinCinar <ayten_baha@hotmail.com>
2024-01-16 12:12:07 +0300

    push

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,6 +39,23 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -146,11 +163,180 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(53, 39);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(94, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 64);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(11, 90);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(52, 90);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.TabIndex = 6;
+            this.label13.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(93, 90);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(11, 115);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 13);
+            this.label15.TabIndex = 6;
+            this.label15.Text = "label15";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(10, 141);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(41, 13);
+            this.label16.TabIndex = 6;
+            this.label16.Text = "label16";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(51, 141);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(41, 13);
+            this.label17.TabIndex = 6;
+            this.label17.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(92, 141);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(41, 13);
+            this.label18.TabIndex = 6;
+            this.label18.Text = "label18";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(10, 169);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(41, 13);
+            this.label19.TabIndex = 6;
+            this.label19.Text = "label19";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(9, 195);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(41, 13);
+            this.label20.TabIndex = 6;
+            this.label20.Text = "label20";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(50, 195);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(41, 13);
+            this.label21.TabIndex = 6;
+            this.label21.Text = "label21";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(91, 195);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(41, 13);
+            this.label22.TabIndex = 6;
+            this.label22.Text = "label22";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(307, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(702, 274);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -183,6 +369,23 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace keyLockGuessGame
 {
@@ -19,7 +21,11 @@ namespace keyLockGuessGame
         }
 
         int[] a = new int[4];
-        int right = 3;
+        int[] b = new int[10];
+        int right = 3, plc, lb;
+
+        Random plcs = new Random();
+        Random label = new Random();
 
         public void genNum()
         {
@@ -35,6 +41,14 @@ namespace keyLockGuessGame
             {
                 goto numGenerate;
             }
+
+            for(int i = 0; i < 3; i++)
+            {
+                listBox1.Items.Add(a[i]);
+            }
+            
+            listBox1.Items.Add("-------------------------------------------------------------------------");
+            
         }
 
         public void reStart()
@@ -50,6 +64,132 @@ namespace keyLockGuessGame
             textBox3.Enabled = true;
             textBox3.Text = "";
         }
+
+        public void labels()
+        {
+            label7.Text = "Number is correct and placed well";
+            label11.Text = "Nothing is right";
+            label15.Text = "One number is correct but wrongly placed";
+            label19.Text = "Two number are correct but wrongly placed";
+        }
+
+        public void hints()
+        {
+            Random rnd = new Random();
+            
+        hintsNumGen:
+            for(int i = 0;i < 9; i++)
+            {
+                b[i] = rnd.Next(1,10);
+            }
+
+            if (a[0] != b[0] || a[1] != b[1] || a[2] != b[2])
+            {
+                goto hintsNumGen;
+            }
+
+            // Hints
+
+            places();
+
+            int l1, l2, l3;
+
+            //label 7
+
+            if(plc == 1)
+            {
+                label8.Text = Convert.ToString(a[0]);
+                l3 = int.Parse(label8.Text);
+                lbl();
+                label9.Text = Convert.ToString(b[lb]);
+                l1 = int.Parse(label9.Text);
+                
+            lbl:
+                lbl();
+                if(lb == l1)
+                {
+                    goto lbl;
+                }
+                else
+                {
+                    l2 = b[lb];
+                    label10.Text = Convert.ToString(b[lb]);
+                }                
+            }
+            else if(plc == 2)
+            {
+                label9.Text = Convert.ToString(a[1]);
+                l3 = int.Parse(label9.Text);
+                lbl();
+                label8.Text = Convert.ToString(b[lb]);
+                l1 = int.Parse(label8.Text);
+                
+            lbl:
+                lbl();
+                if (lb == l1)
+                {
+                    goto lbl;
+                }
+                else
+                {
+                    l2 = b[lb];
+                    label10.Text = Convert.ToString(b[lb]);
+                }
+            }
+            else
+            {
+                label10.Text = Convert.ToString(a[2]);
+                l3 = int.Parse(label10.Text);
+                lbl();
+                label8.Text = Convert.ToString(b[lb]);
+                l1 = int.Parse(label8.Text);
+                
+            lbl:
+                lbl();
+                if (lb == l1)
+                {
+                    goto lbl;
+                }
+                else
+                {
+                    l2 = b[lb];
+                    label9.Text = Convert.ToString(b[lb]);
+                }
+            }
+
+            //label 11
+
+            
+            label12.Text = Convert.ToString(l1);
+            label13.Text = Convert.ToString(l2);
+            
+        plc:
+            places();
+            if (lb == l1 || lb == l2)
+            {
+                goto plc;    
+            }
+            else
+            {
+                label14.Text = Convert.ToString(b[lb]);
+            }
+
+            for (int i = 0; i < b.Length - 1; i++)
+            {
+                listBox1.Items.Add(b[i]);
+            }
+        }
+
+        public void places()
+        { 
+            plc = plcs.Next(1,4);
+        }
+
+        public void lbl()
+        {
+            lb = label.Next(4, 7);
+        }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -63,6 +203,8 @@ namespace keyLockGuessGame
         public void Form1_Load(object sender, EventArgs e)
         {
             genNum();
+            hints();
+            labels();
             reStart();
 
             label6.Text = Convert.ToString(right);
