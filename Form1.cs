9cbcb455ed37b4d5423474b6a33722ab19db5344
BahattinCinar <ayten_baha@hotmail.com>
2024-01-19 16:38:36 +0300

    rightNums

--- Form1.cs
+++ Form1.cs
@@ -19,5 +19,81 @@ namespace keyLockGuessGame
         {
             InitializeComponent();
         }
+
+        int[] a = new int[3];
+
+        public void rightNums()
+        {
+            Random RightNums = new Random();
+
+            int rndm;
+
+            for (int i = 0; i < 3; i++) 
+            {
+            rndm:
+                rndm = RightNums.Next(1,10);
+
+                int aof = Array.IndexOf(a, rndm);
+
+                if (aof == -1)
+                {
+                    a[i] = rndm;
+                }
+                else
+                {
+                    goto rndm;
+                }
+            }
+        }
+
+        public void hints()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int text1,text2,text3;
+
+            text1 = int.Parse(textBox1.Text);
+            text2 = int.Parse(textBox2.Text);
+            text3 = int.Parse(textBox3.Text);
+
+            if (text1 == a[0])
+            {
+                textBox1.Enabled = false;
+                textBox1.BackColor = Color.White;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+                textBox1.ForeColor = Color.Black;
+            }
+            if (text2 == a[1])
+            {
+                textBox2.Enabled = false;
+                textBox2.BackColor = Color.White;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+                textBox2.ForeColor = Color.Black;
+            }
+            if (text3 == a[2])
+            {
+                textBox3.Enabled = false;
+                textBox3.BackColor = Color.White;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+                textBox3.ForeColor = Color.Black;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rightNums();
+        }
     }
 }
